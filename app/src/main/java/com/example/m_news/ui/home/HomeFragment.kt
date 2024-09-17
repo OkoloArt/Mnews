a67aa04d7e98370d3ec54d97f47e4fcb31dab7fa
Arthur <okoloarthur4@gmail.com>
2024-09-17 05:49:16 +0100

    refactor home fragment

--- app/src/main/java/com/example/m_news/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/m_news/ui/home/HomeFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
@@ -59,7 +58,6 @@ class HomeFragment : Fragment() {
                 findNavController().navigate(action)
             },
             onBookmarkClick = {news ->
-                Toast.makeText(requireContext(), news.isBookmarked.toString(), Toast.LENGTH_SHORT).show()
                 val position = newsListAdapter.currentList.indexOf(news)
                 homeViewModel.toggleBookmark(news)
                 newsListAdapter.notifyItemChanged(position)
